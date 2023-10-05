1c6ef24a598c0950ce3c37c60ad5c878eb988faf
Shannon Ioffe <109975145+shanioffe@users.noreply.github.com>
2023-10-05 17:37:08 -0400

    add server

---
+++ adopt-me-app/backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// Secret key for JWT (replace with your own secret)
+const JWT_SECRET = 'your-secret-key';
+
+// In-memory user database (replace with a real database)
+const users = [
+  { id: 1, username: 'user', password: 'password' },
+  // Add more users as needed
+];
+
+// Login route
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Find user by username (in-memory database)
+  const user = users.find((u) => u.username === username);
+
+  if (!user || user.password !== password) {
+    res.status(401).json({ message: 'Authentication failed' });
+    return;
+  }
+
+  // Create a JWT token for the authenticated user
+  const token = jwt.sign({ username: user.username }, JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  // Send the token as a response
+  res.json({ token });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
