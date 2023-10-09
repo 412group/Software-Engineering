437c95d36ecb4bb28fe21de11eca12e04b8d7ac8
Shannon Ioffe <109975145+shanioffe@users.noreply.github.com>
2023-10-09 15:46:25 -0400

    new app configuration

---
+++ adopt-me/frontend-components/pet-profile-container/pet-profile-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-profile-container',
+  templateUrl: './pet-profile-container.component.html',
+  styleUrls: ['./pet-profile-container.component.css']
+})
+export class PetProfileContainerComponent {
+
+}
