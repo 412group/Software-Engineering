928311e63841bbf98fa2eb99c1ceeac71e975758
Shannon Ioffe <109975145+shanioffe@users.noreply.github.com>
2023-10-09 15:35:18 -0400

    add primary navpar component

--- adopt-me-app/frontend/src/app/app.module.ts
+++ adopt-me-app/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimaryNavbarComponent } from './primary-navbar/primary-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryNavbarComponent
   ],
   imports: [
     BrowserModule,
