66243628a500755e34029d44704b02f78fdec234
Shannon Ioffe <109975145+shanioffe@users.noreply.github.com>
2023-10-30 13:26:48 -0400

    add homepage

---
+++ adopt-me/src/app/backend/pet.model.ts
@@ -0,0 +1,11 @@
+export interface Pet {
+    _id: string;
+    Animal: string;
+    Name: string;
+    Breed: string;
+    Image: string; // URL to the pet's image
+    Shelter: string; // Shelter information
+    Description: string; // Description of the pet
+    Age: number; // Age of the pet in months
+  }
+  
