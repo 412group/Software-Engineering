9d5c7e781a851fc402ee156843ee9ded734cbebc
Alley Williams <alleywilliams96@gmail.com>
2023-11-27 13:36:26 -0500

    login page update, server.js update

--- server.js
+++ server.js
@@ -1,74 +1,93 @@
 const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-
-const db = mongoose.connection;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-mongoose.connect('mongodb://localhost/adopt-me.pets', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Connect to MongoDB Atlas database
+const uri = 'mongodb+srv://Allwill123:hello123@cluster0.hcsvskr.mongodb.net/AdoptMe';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+db.once('open', function () {
+  console.log('Connected to MongoDB Atlas database');
 });
 
-const bookSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  published: Date,
+// Parse incoming requests
+app.use(bodyParser.json());
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'adopt-mev2')));
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
 });
 
-const Book = mongoose.model('Book', bookSchema);
+// Create the User model
+const User = mongoose.model('User', userSchema);
 
-const newBook = new Book({
-  title: 'Sample Book',
-  author: 'John Doe',
-  published: new Date(),
-});
+// Signup route
+app.post('/signup', async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
-newBook.save((err, book) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Book saved:', book);
+  // Validate user input
+  if (!username || !email || !password) {
+    res.status(400).send('Missing required fields');
+    return;
   }
-});
 
-Book.find({ author: 'John Doe' }, (err, books) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Books by John Doe:', books);
-  }
-});
+  try {
+    // Check if email or username already exists in the database
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
 
-Book.updateOne(
-  { title: 'Sample Book' },
-  { author: 'Jane Smith' },
-  (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Book updated:', result);
+    if (existingUser) {
+      res.status(400).send('Email or username already exists');
+      return;
     }
-  }
-);
 
-Book.deleteOne({ title: 'Sample Book' }, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Book deleted');
+  // Create a new user document
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
+
+  // Add the user to the database
+    await newUser.save();
+    res.status(201).send('User created successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
   }
+});
+
+// Login route
+app.post('/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const user = await User.findOne({ email, password });
+
+    if (user) {
+      res.status(200).send('Login successful');
+    } else {
+      res.status(401).send('Invalid email or password');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
